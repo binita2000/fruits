b6ecf774aededfed868145a35c26e2a178e0a8b4
aarati khadka <aaratik2058@gmail.com>
2024-07-17 15:53:29 +0545

    changes

--- admin/layouts/sidebar.php
+++ admin/layouts/sidebar.php
@@ -235,25 +235,7 @@
                 </li>
             </ul>
         </li>
-        <li class="menu-item">
-            <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bx-layout"></i>
-                <div data-i18n="Layouts">category</div>
-            </a>
-
-            <ul class="menu-sub">
-                <li class="menu-item">
-                    <a href="../categories/create.php" class="menu-link">
-                        <div data-i18n="Without menu">Add category</div>
-                    </a>
-                </li>
-                <li class="menu-item">
-                    <a href="../categories/index.php" class="menu-link">
-                        <div data-i18n="Without navbar">Manage category</div>
-                    </a>
-                </li>
-            </ul>
-        </li>
+        
 
        
         <li class="menu-item">
